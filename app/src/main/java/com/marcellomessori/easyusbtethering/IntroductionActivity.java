77752e9665fd420969e7c63b3b277b69e445933d
Sakshum Sharma <sakssharma@expedia.com>
2018-10-28 20:24:26 +0530

    Changed look a little

--- app/src/main/java/com/marcellomessori/easyusbtethering/IntroductionActivity.java
+++ app/src/main/java/com/marcellomessori/easyusbtethering/IntroductionActivity.java
@@ -1,8 +1,11 @@
 package com.marcellomessori.easyusbtethering;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -12,6 +15,7 @@ public class IntroductionActivity extends Activity {
 
 
     private AdView mAdView;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +26,17 @@ public class IntroductionActivity extends Activity {
         mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        mButton = (Button) findViewById(R.id.dummy_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tetherSettings = new Intent();
+                tetherSettings.setClassName("com.android.settings", "com.android.settings.TetherSettings");
+                tetherSettings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(tetherSettings);
+            }
+        });
     }
 
-    public void closeActivity(View view) {
-        finish();
-    }
 }
